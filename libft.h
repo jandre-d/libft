824ae584aa92714fd2664b6a99eaa2029040719d
jandre-d <jandre-d@student.codam.nl>
2019-04-30 17:33:09 +0200

    fix norm

--- libft.h
+++ libft.h
@@ -6,15 +6,15 @@
 /*   By: jandre-d <jandre-d@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/30 16:26:13 by jandre-d       #+#    #+#                */
-/*   Updated: 2019/04/30 16:26:32 by jandre-d      ########   odam.nl         */
+/*   Updated: 2019/04/30 17:32:27 by jandre-d      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-#include "includes/libft_base.h"
-#include "includes/get_next_line.h"
-#include "includes/ft_printf.h"
+# include "includes/libft_base.h"
+# include "includes/get_next_line.h"
+# include "includes/ft_printf.h"
 
 #endif
