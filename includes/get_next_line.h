6bae563068566e1fe4bf20abb143ce02afd68a92
jandre-d <jandre-d@student.codam.nl>
2019-04-30 17:31:09 +0200

    ^@

---
+++ includes/get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.h                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jandre-d <jandre-d@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/01/19 10:48:53 by jandre-d       #+#    #+#                */
+/*   Updated: 2019/04/29 20:21:05 by jandre-d      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 42
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft.h"
+
+typedef struct			s_fd_list
+{
+	ssize_t				fd;
+	ssize_t				buffer_usage;
+	char				*buffer;
+	struct s_fd_list	*next;
+}						t_fd_list;
+
+int						get_next_line(const int fd, char **line);
+
+#endif
